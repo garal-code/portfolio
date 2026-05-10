f8d1a320aaafe3329f11d81e7672b525fc3c5ccc
javiergarciaalvarez95 <javier.garcia.alvarez95@gmail.com>
2026-05-10 14:34:49 +0200

    fix(hosting): normalize pages base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://garal-code.github.io/portfolio",
-  base: "/portfolio",
+  site: "https://garal-code.github.io/portfolio/",
+  base: "/portfolio/",
 
   vite: {
     plugins: [tailwindcss()]
